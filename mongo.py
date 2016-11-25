d3e5f9c06abf092413cf29ae6fd2d42a8696d9f8
Bryan Gilbertson <gilby125@users.noreply.github.com>
2016-11-25 15:50:39 -0600

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -17,7 +17,8 @@ def init():
     #     client = MongoClient('localhost:27017')
     #     db = client['icarus']
     # else:   # MongoHQ db
-    client = MongoClient('MONGODB_URI')
+    client = MongoClient(os.environ.get('MONGODB_URI'))
+    #client = MongoClient('MONGODB_URI')
     db = client['icarus']
         
     collection = db['round_trip']
